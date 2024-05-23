f7cceb86b79af25280e42b3f2bda87f04b182d9c
Afsha Hossain <afsha.hossain@capgemini.com>
2024-05-23 20:56:20 +0100

    Complete all the Workbooks and Challenges from Java Fundamentals to Variables Section 1 to 4.

---
+++ src/main/java/JoiningStrings.java
@@ -0,0 +1,9 @@
+public class JoiningStrings {
+    public static void main(String[] args) {
+        int year = 2010;
+        String winner = "Spain";
+
+        String announcement = "The winner of the " + year + " world cup is " + winner + " team";
+        System.out.println(announcement);
+    }
+}
---
+++ src/main/java/WorkbookTwo.java
@@ -0,0 +1,54 @@
+//public class WorkbookTwo {
+//    public static void main(String[] args) {
+//        int point = 0;
+//        int negativeFifty = point - 50;
+//        int negativeThree = point - 3;
+//        int plusThirty = point + 30;
+//        int plusHundred = point + 100;
+//        int plusSixty = point + 60;
+//
+//        System.out.println("Harry was caught wandering the halls." + negativeFifty + " for Gryffindor");
+//        System.out.println("Harry being cheeky in class." + negativeThree + " points for Gryffindor");
+//        System.out.println("Hermione got full marks on Lockhart/'s quiz." + plusThirty + " points for Gryffindor");
+//        System.out.println("Ron won the underground chess game." + plusHundred + " points for Gryffindor");
+//        System.out.println("Harry defeated Quirrell." + plusSixty + " points for Gryffindor");
+//        System.out.println(negativeFifty + negativeThree + plusThirty + plusHundred + plusSixty);
+//    }
+//}
+
+//public class WorkbookTwo {
+//    public static void main(String[] args) {
+//        int point = 0;
+//
+//        System.out.println("Harry was caught wandering the halls. -50 for Gryffindor");
+//        point -= 50;
+//        System.out.println("Harry being cheeky in class. -3 points for Gryffindor");
+//        point -= 3;
+//        System.out.println("Hermione got full marks on Lockhart/'s quiz. 30 points for Gryffindor");
+//        point += 30;
+//        System.out.println("Ron won the underground chess game. 100 points for Gryffindor");
+//        point += 100;
+//        System.out.println("Harry defeated Quirrell. 60 points for Gryffindor");
+//        point+= 60;
+//        System.out.println(point);
+//    }
+//}
+
+
+public class WorkbookTwo {
+    public static void main(String[] args) {
+        String name = "Afsha";
+        int age = 17;
+        String country = "England";
+        String sport = "dancing";
+        int hours = 1;
+        String hobby = "music";
+        String subject = "Physics";
+        char grade = 'B';
+
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+        System.out.println("My favorite sport is " + sport + ". I play for " + hours + " hour(s) a day");
+        System.out.println("When I'm tired, I like to play " + hobby + ".");
+        System.out.println("In school, my favourite subject was " + subject + ". I scored a " + grade + ".");
+    }
+}
